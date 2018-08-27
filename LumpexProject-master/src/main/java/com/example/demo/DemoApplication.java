b9a8cfee04f850ff00b454c6d03dc3498bc76e48
Damian <35864290+DamianJaku877@users.noreply.github.com>
2018-08-27 19:12:44 +0200

    v0.1

--- LumpexProject-master/src/main/java/com/example/demo/DemoApplication.java
+++ LumpexProject-master/src/main/java/com/example/demo/DemoApplication.java
@@ -2,6 +2,10 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @SpringBootApplication
 public class DemoApplication {
@@ -10,5 +14,16 @@ public class DemoApplication {
 
         SpringApplication.run(DemoApplication.class, args);
 
+    }
+
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurerAdapter() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/shops/all").allowedOrigins("http://localhost:63342");
+            }
+        };
     }
 }
