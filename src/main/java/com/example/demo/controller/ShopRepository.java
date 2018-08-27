6eed145d45fdc12e7b1bdb447a87fc7cb27431a3
Damian <35864290+DamianJaku877@users.noreply.github.com>
2018-08-27 18:27:59 +0200

    dasfa

--- src/main/java/com/example/demo/controller/ShopRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.demo.controller;
-
-import com.example.demo.model.Shop;
-import org.springframework.data.repository.CrudRepository;
-
-public interface ShopRepository extends CrudRepository<Shop,Long> {
-
-}
